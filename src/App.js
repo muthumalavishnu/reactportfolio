c4daa15265008bd7e2ed5ded2cf89b0a0738d7ce
muthumalavishnu <muthumalavishnu@gmail.com>
2025-06-02 12:38:40 -0500

    ghp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeScreen from "./HomeScreen";
 import About from "./About";
 import Contact from "./Contact";
